81090365b5f284ff8ea0f19c9a2de66aa9a7a6e7
Mike Covington <mfcovington@gmail.com>
2015-02-26 00:04:40 -0800

    Harvest and log text and hrefs from menu links

--- mobile-dropdown/js/app.js
+++ mobile-dropdown/js/app.js
@@ -1,3 +1,13 @@
 var $select = $('<select></select>');
 
 $('#menu').append($select);
+
+var menuLinks = $('#menu a');
+
+for (var i = 0; i < menuLinks.length; i++) {
+    var currentLink = menuLinks[i];
+    var text = currentLink.text;
+    var href = currentLink.href;
+    console.log(text);
+    console.log(href);
+};
