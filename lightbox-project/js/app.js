4707e9eb84a91a688c525b6919a01d465121d432
Mike Covington <mfcovington@gmail.com>
2015-02-25 20:50:08 -0800

    Log alt of clicked thumbnail

--- lightbox-project/js/app.js
+++ lightbox-project/js/app.js
@@ -1,5 +1,7 @@
 $("#imageGallery a").click(function () {
     event.preventDefault();
     var href = $(this).attr("href");
+    var alt = $(this).children('img').attr('alt');
     console.log(href);
+    console.log(alt);
 });
