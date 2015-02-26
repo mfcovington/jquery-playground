55bf39782fc3d0d24b9bc1b0e7f31f9c9d1e0a32
Mike Covington <mfcovington@gmail.com>
2015-02-26 00:16:18 -0800

    Get link text and href in more proper jQuery manner

--- mobile-dropdown/js/app.js
+++ mobile-dropdown/js/app.js
@@ -5,9 +5,9 @@ $('#menu').append($select);
 var menuLinks = $('#menu a');
 
 for (var i = 0; i < menuLinks.length; i++) {
-    var currentLink = menuLinks[i];
-    var text = currentLink.text;
-    var href = currentLink.href;
+    var $currentLink = $(menuLinks[i]);
+    var text = $currentLink.text();
+    var href = $currentLink.attr('href');
 
     var $option = $('<option></option>');
     $option.text(text);
