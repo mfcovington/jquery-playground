03e453fde6af23900e4abcfca40cfe5755512fd4
Mike Covington <mfcovington@gmail.com>
2015-02-26 13:06:45 -0800

    Use val() instead of attr('value')

--- mobile-dropdown/js/app.js
+++ mobile-dropdown/js/app.js
@@ -9,7 +9,7 @@ $('#menu li').each(function() {
 
     var $option = $('<option></option>');
     $option.text(text);
-    $option.attr('value', href);
+    $option.val(href);
 
     if ($(this).hasClass('selected')) {
         $option.prop('selected', 'selected');
@@ -22,14 +22,14 @@ $('#menu li').each(function() {
 var $form = $('<form method="link"></form>');
 var $button = $('<button>Go</button>');
 
-var selectedHref = $select.children(":selected").attr('value');
+var selectedHref = $select.children(":selected").val();
 $form.attr('action', selectedHref);
 
 $form.append($button);
 $('#menu').append($form);
 
 $select.change(function() {
-    var selectedHref = $select.children(":selected").attr('value');
+    var selectedHref = $select.children(":selected").val();
     var selectedLabel = $select.children(":selected").text();
 
     var message = 'Selection changed to ' + selectedLabel;
