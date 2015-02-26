b65b447fddd820a9372d35bc038a9f8a1b72b4e5
Mike Covington <mfcovington@gmail.com>
2015-02-26 00:06:49 -0800

    Append options (with text and href) to select

--- mobile-dropdown/js/app.js
+++ mobile-dropdown/js/app.js
@@ -8,4 +8,9 @@ for (var i = 0; i < menuLinks.length; i++) {
     var currentLink = menuLinks[i];
     var text = currentLink.text;
     var href = currentLink.href;
+
+    var $option = $('<option></option>');
+    $option.text(text);
+    $option.attr('value', href);
+    $select.append($option);
 };
