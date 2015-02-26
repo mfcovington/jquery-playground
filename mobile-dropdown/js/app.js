0d96afade3af749ebb7fcfdd1ea7881fdb9546ef
Mike Covington <mfcovington@gmail.com>
2015-02-26 00:45:33 -0800

    Iterate through menu items using each instead of for

--- mobile-dropdown/js/app.js
+++ mobile-dropdown/js/app.js
@@ -2,10 +2,8 @@ var $select = $('<select></select>');
 
 $('#menu').append($select);
 
-var menuList = $('#menu li');
-
-for (var i = 0; i < menuList.length; i++) {
-    var $menuLink = $(menuList[i]).children('a');
+$('#menu li').each(function() {
+    var $menuLink = $(this).children('a');
     var text = $menuLink.text();
     var href = $menuLink.attr('href');
 
@@ -13,4 +11,4 @@ for (var i = 0; i < menuList.length; i++) {
     $option.text(text);
     $option.attr('value', href);
     $select.append($option);
-};
+});
