4614a9ebf5e21463bb5d00e1693925a4266e091e
Mike Covington <mfcovington@gmail.com>
2015-02-27 01:53:01 -0800

    Toggle/hide color selector (depending on button clicked)

--- draw/js/app.js
+++ draw/js/app.js
@@ -3,6 +3,14 @@ $('.controls li').click(function() {
     selectColor($(this));
 });
 
+$('#revealColorSelect').click(function() {
+    $('#colorSelect').toggle();
+});
+
+$('#addNewColor').click(function() {
+    $('#colorSelect').hide();
+});
+
 function deselectColors() {
     $('.controls li').siblings('li').each(function() {
         $(this).removeClass('selected')
