472890b9ded9614634a6f5e7f913084d1bc2b1f1
Mike Covington <mfcovington@gmail.com>
2015-02-26 22:34:57 -0800

    Define functions to checkPasswordLength() and checkPasswordConfirm()

--- password-confirmation/js/app.js
+++ password-confirmation/js/app.js
@@ -1,27 +1,29 @@
 var $passwordInput = $('#password');
 var $passwordConfirm = $('#confirm_password');
 
-$passwordInput.keyup(function() {
+function checkPasswordLength() {
     if ($passwordInput.val().length > 8) {
         $passwordInput.siblings('span').hide();
     }
     else {
         $passwordInput.siblings('span').show();
     }
+}
 
+function checkPasswordConfirm(argument) {
     if ($passwordInput.val() === $passwordConfirm.val()) {
         $passwordConfirm.siblings('span').hide();
     }
     else {
         $passwordConfirm.siblings('span').show();
     }
+}
+
+$passwordInput.keyup(function() {
+    checkPasswordLength();
+    checkPasswordConfirm();
 });
 
 $passwordConfirm.keyup(function() {
-    if ($passwordInput.val() === $passwordConfirm.val()) {
-        $passwordConfirm.siblings('span').hide();
-    }
-    else {
-        $passwordConfirm.siblings('span').show();
-    }
+    checkPasswordConfirm();
 });
