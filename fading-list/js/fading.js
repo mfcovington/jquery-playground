a7c5a87ade6bc87a0ef9bf0c9af7f9874f829aef
Mike Covington <mfcovington@gmail.com>
2015-03-19 23:00:15 -0700

    Start fade at an opacity of 1

--- fading-list/js/fading.js
+++ fading-list/js/fading.js
@@ -4,7 +4,7 @@ var item_count = $list_items.length;
 
 var item_number = 0;
 $list_items.each(function(){
-    item_number++;
     var $item = $(this);
     $item.css('opacity', 1 - item_number / item_count);
+    item_number++;
 });
