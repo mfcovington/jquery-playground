d0e76539e0cc920a82324ff4bfc750d17482b3de
Mike Covington <mfcovington@gmail.com>
2015-03-19 23:17:05 -0700

    Click to change opacity peak

--- fading-list/js/fading.js
+++ fading-list/js/fading.js
@@ -8,3 +8,26 @@ $list_items.each(function(){
     $item.css('opacity', 1 - item_number / item_count);
     item_number++;
 });
+
+$fading_list.on('click', 'li', function(event) {
+    event.preventDefault();
+
+    var $clicked_item = $(this);
+    console.log($clicked_item.css('opacity'));
+
+    $clicked_item.css('opacity', 1);
+
+    var item_number = 1;
+    $clicked_item.nextAll('li').each(function(){
+        var $item = $(this);
+        $item.css('opacity', 1 - item_number / item_count);
+        item_number++;
+    });
+
+    var item_number = 1;
+    $clicked_item.prevAll('li').each(function(){
+        var $item = $(this);
+        $item.css('opacity', 1 - item_number / item_count);
+        item_number++;
+    });
+});
