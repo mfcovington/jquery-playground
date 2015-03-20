f2728b645de622cdc041413b4c35676cd3a00aaa
Mike Covington <mfcovington@gmail.com>
2015-03-19 22:49:45 -0700

    Make fading list

---
+++ fading-list/js/fading.js
@@ -0,0 +1,10 @@
+var $fading_list = $('.fading-list');
+var $list_items = $fading_list.find('li')
+var item_count = $list_items.length;
+
+var item_number = 0;
+$list_items.each(function(){
+    item_number++;
+    var $item = $(this);
+    $item.css('opacity', 1 - item_number / item_count);
+});
