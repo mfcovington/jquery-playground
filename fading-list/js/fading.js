d772304f1155c6ea3b837d40701c8d831ee50b69
Mike Covington <mfcovington@gmail.com>
2015-03-20 00:51:40 -0700

    Don't log opacity to console

--- fading-list/js/fading.js
+++ fading-list/js/fading.js
@@ -9,10 +9,6 @@ fadeListItems($listItems, 0, itemCount);
 
 $fadingList.on('mouseover', 'li', function() {
     var $clickedItem = $(this);
-
-    var clickedOpacity = $clickedItem.css('opacity');
-    console.log('Opacity: ' + clickedOpacity);
-
     $clickedItem.css('opacity', 1);
     fadeListItems($clickedItem.nextAll('li'), 1, itemCount);
     fadeListItems($clickedItem.prevAll('li'), 1, itemCount);
@@ -38,6 +34,5 @@ function randomColor() {
     var r = Math.floor(256 * Math.random());
     var g = Math.floor(256 * Math.random());
     var b = Math.floor(256 * Math.random());
-    // return [ r, g, b ];
     return 'rgb(' + r + ', ' + g + ', ' + b + ')' ;
 }
